758f140ca6b97083cc5fc8be92829e5865a0733e
aguinavarro <aguinaldon@nevada.unr.edu>
2021-04-26 12:50:41 -0700

    compacted code

--- numbernodes.cpp
+++ numbernodes.cpp
@@ -12,29 +12,29 @@
 using namespace std;
 
 int numnodes = 10;
+int totalenergy = 0;
 
 void recursivenodes(int connections, int distance);
 
 int main() {
-  int dist = 0;
   srand(time(NULL));
-  recursivenodes(initialconnections, dist);
+  recursivenodes(initialconnections, 0);
   cout << "Number of nodes: " << numnodes << endl;
+  cout << "Total energy: " << totalenergy << endl;
   return 0;
 }
 
 void recursivenodes(int connections, int distance) {
-  if (connections <= 1) {
+  if (connections <= 1)
     return;
-  }
 
   for (int i = 0; i < connections; i++) {
     int dummy = (int)(connections/(pow(2.0, distance)));
-    if (dummy == 0) {
+    if (dummy == 0)
       return;
-    }
     int connect = rand() % dummy + 0;
     numnodes += connect;
+    totalenergy += (connect+1)*10;
     recursivenodes(connect, distance+1);
   }
   return;
