c39369f3b70e6315989670d4e613cec28050cca8
aguinavarro <aguinaldon@nevada.unr.edu>
2021-04-27 13:09:21 -0700

    network is tested, #nodes in network matches theoretical #nodes

--- main.cpp
+++ main.cpp
@@ -6,13 +6,14 @@
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
+#include <iomanip>
 
 #define INITIAL_CONNECTIONS 10
 
 using namespace std;
 
 int totalindex = 0;
-int numnodes = INITIAL_CONNECTIONS;
+int numnodes = -1;
 int totalenergy = 0;
 float bandwith = 0.0;
 
@@ -35,6 +36,8 @@ SensorNetwork * create();
 void remove(SensorNetwork * network);
 void add(SensorNetwork * network, int connections, int distance, float rate);
 void del(SensorNetwork * network);
+void printnetwork(SensorNetwork * network);
+void printnode(Node * node);
 Node * find(SensorNetwork * network, int index);
 void recursivenodes(SensorNetwork * network, int connections, int distance,
                     float rate);
@@ -45,6 +48,17 @@ int main() {
   recursivenodes(network, INITIAL_CONNECTIONS, 0, 1);
   cout << "Number of nodes: " << numnodes << endl;
   cout << "Total energy: " << totalenergy << endl;
+
+  int random = rand() % numnodes + 0;
+  /*cout << "Finding node " << random << "..." << endl;
+  Node * randomnode = find(network, random);
+  if (!randomnode) {
+  } else {
+    printnode(randomnode);
+  }*/
+
+  cout << "Printing network..." << endl;
+  printnetwork(network);
   return 0;
 }
 
@@ -68,7 +82,7 @@ void add(SensorNetwork * network, int connections, int distance, float rate) {
   Node * newnode = new Node;
   newnode->index = totalindex++;
   newnode->numconnections = connections;
-  newnode->maxenergy = connections * 10;
+  newnode->maxenergy = (connections+1) * 10;
   newnode->distance = distance;
   newnode->rate = rate;
   newnode->power = true;
@@ -95,6 +109,32 @@ void del(SensorNetwork * network) {
   }
 }
 
+void printnetwork(SensorNetwork * network) {
+  if (!network) {
+    cout << "No network to print." << endl;
+    return;
+  } else {
+    Node * ptr = network->front;
+    while (ptr && ptr->next) {
+      cout << "Index: " << setw(5) << left << ptr->index;
+      cout << " Connections: " << setw(5) << left << ptr->numconnections;
+      cout << " Max energy: " << setw(5) << left << ptr->maxenergy;
+      cout << " Distance: " << setw(5) << left << ptr->distance;
+      cout << " Rate: " << setw(5) << left << ptr->rate << endl;
+      ptr = ptr->next;
+    }
+  }
+  return;
+}
+
+void printnode(Node * node) {
+  cout << "Index: " << setw(3) << left << node->index;
+  cout << " Connections: " << setw(2) << left << node->numconnections;
+  cout << " Max energy: " << setw(3) << left << node->maxenergy;
+  cout << " Distance: " << setw(2) << left << node->distance;
+  cout << " Rate: " << setw(5) << left << node->rate << endl;
+}
+
 Node * find(SensorNetwork * network, int index) {
   if (!network)
     return NULL;
@@ -112,18 +152,22 @@ Node * find(SensorNetwork * network, int index) {
 
 void recursivenodes(SensorNetwork * network, int connections, int distance,
                     float rate) {
-  if (connections <= 1)
+  numnodes++;
+  if (connections == 0) {
     return;
-
+  }
+  if (connections == INITIAL_CONNECTIONS) {
+    add(network, connections, distance, rate);
+    distance++;
+  }
   for (int i = 0; i < connections; i++) {
-    int dummy = (int)(connections/(pow(2.0, distance)));
+    int dummy = (int)(INITIAL_CONNECTIONS/(pow(2.0, distance)));
     if (dummy == 0)
       return;
-    int connect = rand() % dummy + 0;
-    add(network, connect, distance, rate);
-    numnodes += connect;
-    totalenergy += (connect+1)*10;
-    recursivenodes(network, connect, distance+1, rate/2);
+    int newconnections = rand() % dummy + 0;
+    add(network, newconnections, distance, rate/2);
+    totalenergy += (newconnections+1)*10;
+    recursivenodes(network, newconnections, distance+1, rate/2);
   }
   return;
 }
